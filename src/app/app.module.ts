15eae04ebc82b0c9a6291bebc24017626a7075f3
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-01-04 01:51:37 +0530

    2 way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Test1Component } from './test1/test1.component';
@@ -12,7 +12,8 @@ import { Test1Component } from './test1/test1.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -2,29 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test1',
-  template: `<h3 class = "text-danger">{{name}}</h3>
-             <button (click)="onClick($event)">Greet</button>
-             {{greeting}}
+  template: `<input [(ngModel)]="name" type="text" placeholder="Enter your name">
+  {{name}}
 `,
-  styles: [`
-    .text-danger {
-      color : red;
-    }
-    .text-success {
-      color : green;
-    }
-    .text-special {
-      font-style : italic;
-    }
-
-  `]
+  styles: []
 })
 export class Test1Component {
-  public name = "Subhro";
-  public greeting = "";
-  onClick(event: any) {
-    console.log(event);
-    this.greeting = "Wecome " + this.name;
-  }
-  }
+  public name = "";
+  
+}
+  
 
