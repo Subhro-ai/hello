2db492a5955362e9b0f56aa22a981a4175abeb5f
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-01-04 02:05:10 +0530

    Template reference variable

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test1',
-  template: `<input [(ngModel)]="name" type="text" placeholder="Enter your name">
-  {{name}}
+  template: `<input #myName type="text" placeholder="Enter your name">
+              <button (click)="logMessage(myName.value)">Log</button>
 `,
   styles: []
 })
 export class Test1Component {
   public name = "";
-  
+  logMessage (value: any) {
+    console.log(value);
+  }
 }
   
 
