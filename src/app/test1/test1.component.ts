ec04bbb0e91324a12ad1a0ee3470e6b526e47954
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-01-04 01:41:01 +0530

    Event binding

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-test1',
   template: `<h3 class = "text-danger">{{name}}</h3>
-             <h1 [style.color] = "hasError ? 'green' : 'red'">Style Binding </h1>
-             <h2 [style.color] = "highlight">Highlight</h2>
+             <button (click)="onClick($event)">Greet</button>
+             {{greeting}}
 `,
   styles: [`
     .text-danger {
@@ -21,9 +21,10 @@ import { Component } from '@angular/core';
 })
 export class Test1Component {
   public name = "Subhro";
-  public className = "text-success";
-  public hasError = false;
-  public isSpecial = true;
-  public highlight = "orange";
+  public greeting = "";
+  onClick(event: any) {
+    console.log(event);
+    this.greeting = "Wecome " + this.name;
+  }
   }
 
