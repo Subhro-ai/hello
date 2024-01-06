f8c3450ec947f549eac32e70b67df82c292f4eac
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-01-07 01:19:23 +0530

    ngIf 1

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -2,16 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-test1',
-  template: `<input #myName type="text" placeholder="Enter your name">
-              <button (click)="logMessage(myName.value)">Log</button>
+  template: `<h2 *ngIf="0">Welcome</h2>
 `,
   styles: []
 })
 export class Test1Component {
-  public name = "";
-  logMessage (value: any) {
-    console.log(value);
-  }
+  
 }
+
   
 
