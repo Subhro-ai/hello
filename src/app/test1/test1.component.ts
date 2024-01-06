83bf3254e86a92002d8009452e0a78905ec796de
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-01-07 01:28:21 +0530

    ngIf 3

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -3,11 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-test1',
   template: `<h2 *ngIf="displayName">Welcome</h2>
+              <button (click)="change()">Change</button>
 `,
   styles: []
 })
 export class Test1Component {
   public displayName = false
+  change() {
+    this.displayName = !this.displayName;
+  }
 }
 
   
