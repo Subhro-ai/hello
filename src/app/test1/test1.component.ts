d658e4f7779ea53b4cf96a23ae184b20291ec6c7
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-01-07 01:59:12 +0530

    ngFor

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -3,23 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-test1',
   template: `
-              <input #colorchoice type="text">
-              <button (click)="changeColor(colorchoice.value)">Enter</button>
-              <div [ngSwitch]="color">
-              <div *ngSwitchCase="'red'">RED</div>
-              <div *ngSwitchCase="'blue'">BLUE</div>
-              <div *ngSwitchCase="'green'">GREEN</div>
-              <div *ngSwitchDefault>Pick again!</div>
-              </div>
+            <div *ngFor="let color of colors">
+            <h2>{{color}}</h2>
+            </div>
+              
 `,
   styles: []
 })
 export class Test1Component {
-  public color = "green";
-  changeColor(colorc:any) {
-    this.color = colorc;
-    console.log(this.color);
-  }
+  public colors = ['red', 'green', 'blue', 'yellow'];
 }
 
   
