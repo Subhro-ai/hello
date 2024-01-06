641a0cc78477f82c0b24654df9cc994c3c641e09
Subhro-ai <subhrajyoti.singha30@gmail.com>
2024-01-07 02:03:47 +0530

    ngFor index

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-test1',
   template: `
-            <div *ngFor="let color of colors">
-            <h2>{{color}}</h2>
+            <div *ngFor="let color of colors; index as i">
+            <h2> {{i+1}}. {{color}}</h2>
             </div>
               
 `,
