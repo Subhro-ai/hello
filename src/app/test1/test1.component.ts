557709c791bb90a5c344f36c2fe9de7f46fdf1f1
Subhrajyoti Singha <subhrajyoti.singha30@gmail.com>
2024-01-10 00:28:29 +0530

    Pipes date

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -9,15 +9,22 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
             <h2> {{name | uppercase}} </h2>
             <h2> {{name | titlecase}} </h2>
             <h2> {{name | slice:3:5}} </h2>
-            <h2> {{5.12 | number :'1.2-3'}} </h2>
+            <h2> {{5.12 | number :'1.0-1'}} </h2>
             <h2> {{5.12 | number :'2.4-5'}} </h2>
             <h2> {{5.12 | number :'3.4-6'}} </h2>
+            <h2> {{0.25 | percent}} </h2>
+            <h2> {{0.25 | currency}} </h2>
+            <h2> {{0.25 | currency:'INR'}} </h2>
+            <h2> {{date | date:'short'}} </h2>
+            <h2> {{date | date:'shortDate'}} </h2>
+            <h2> {{date | date:'shortTime'}} </h2>
               
 `,
   styles: []
 })
 export class Test1Component {
   public name = "Bruce wayne";
+  public date = new Date();
 }
 
   
