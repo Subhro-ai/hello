90943d39fa04b7b65c6d0c8c7ee09e069fa6b3b2
Subhrajyoti Singha <subhrajyoti.singha30@gmail.com>
2024-01-10 00:09:23 +0530

    Child to parent interaction

--- src/app/test1/test1.component.ts
+++ src/app/test1/test1.component.ts
@@ -1,17 +1,23 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-test1',
   template: `
             
-            <h2> {{name}} </h2>
-            
+            <h2> {{"Hello " + name}} </h2>
+            <button (click)="fireEvent()">Click</button>
               
 `,
   styles: []
 })
 export class Test1Component {
   @Input('parentData') public name: any;
+
+  @Output() public childEvent = new EventEmitter();
+
+  fireEvent() {
+    this.childEvent.emit('Hey Subhro');
+  }
 }
 
   
